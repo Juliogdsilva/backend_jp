54df46c5186a69f73da0fce70811a22d2bcbec46
macbook pro <julio.gdsilva@gmail.com>
2023-03-09 08:59:40 -0300

    bugfix name

--- src/controllers/participants.js
+++ src/controllers/participants.js
@@ -140,7 +140,7 @@ module.exports = (app) => {
     participant.status = 'ative';
     participant.updated_at = new Date();
 
-    sendMailComplet(...participant);
+    sendMailComplet({ ...participant });
 
     delete participant.id;
     await app.db('participants')
