325ef662a5df16fc01263c7465e3825aa5966f98
macbook pro <julio.gdsilva@gmail.com>
2023-03-21 09:11:29 -0300

    chat

---
+++ migrations/20230320203310_create_chat.js
@@ -0,0 +1,23 @@
+const tableName = 'chat';
+
+/* eslint-disable func-names */
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable(tableName, (t) => {
+    t.increments('id').primary().unsigned();
+    t.string('first_name', 150).notNullable();
+    t.text('text').nullable();
+    t.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable(tableName);
+};
--- src/config/socket.js
+++ src/config/socket.js
@@ -1,10 +1,34 @@
-module.exports = (app, io) => {
+module.exports = async (app, io) => {
   console.log('Init socket.io');
   io.on('connection', (socket) => {
-    // console.log('A user connected'); Usuario conectou
+    // Get last message
+    app.db('chat')
+      .select()
+      .orderBy('id', 'desc')
+      .limit(2)
+      .then((data) => io.to(socket.id).emit('latestMensages', data));
 
     socket.on('sendMessage', (data) => {
+      // save New Msg
+      const message = JSON.parse(data);
+      app.db('chat')
+        .insert({ first_name: message.first_name, text: message.text })
+        .then()
+        .catch((err) => {
+          throw err;
+        });
+
+      // Send message for all users
       io.emit('newMessage', data);
+    });
+
+    socket.on('getLatestMensages', (data) => {
+      app.db('chat')
+        .select()
+        .where('id', '<', data)
+        .orderBy('id', 'desc')
+        .limit(10)
+        .then((resp) => io.to(socket.id).emit('moreLatestMensages', resp));
     });
 
     socket.on('disconnect', () => {
