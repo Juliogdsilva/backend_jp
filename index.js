4a4dec5418eb53fe488d172477df59da6ccafc3e
Julio Silva <julio.silva@ironstudiosinc.com>
2023-03-27 10:38:04 -0300

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 require('dotenv').config();
 const app = require('express')();
-const http = require('http').Server(app);
 const consign = require('consign');
-const io = require('socket.io')(http, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['content-type'],
-  },
-});
 
 const port = process.env.PORT || 4020;
 const db = require('./src/config/db');
@@ -17,7 +9,6 @@ app.db = db;
 
 consign()
   .then('./src/config/passport.js')
-  .then('./src/config/socket.js')
   .then('./src/config/middlewares.js')
   .then('./src/config/permission.js')
   .then('./src/tools')
@@ -25,7 +16,7 @@ consign()
   .then('./src/controllers/email.js')
   .then('./src/controllers')
   .then('./src/config/routes.js')
-  .into(app, io);
+  .into(app);
 
 http.listen(port, () => {
   console.log(`Backend rodando na porta ${port}`);
--- src/models/participants.js
+++ src/models/participants.js
@@ -1,35 +1,14 @@
 module.exports = () => {
-  const modelParticipants = (item) => {
-    const participants = {
-      status: item.status,
-      first_name: item.first_name,
-      last_name: item.last_name,
-      email: item.email,
-      phone: item.phone,
-      birthdate: item.birthdate,
-      shoe_number: item.shoe_number,
-      company: item.company,
-      companion: item.companion,
-      document_type: item.document_type,
-      doc_number: item.doc_number,
-      special_needs: item.special_needs,
-      special_needs_describe: item.special_needs_describe,
-      arrival_date: item.arrival_date,
-      arrival_time: item.arrival_time,
-      departure_date: item.departure_date,
-      departure_time: item.arrival_time,
-      flight_number: item.flight_number,
-      flight_number_departure: item.flight_number_departure,
-      have_allergy: item.have_allergy,
-      allergy: item.allergy,
-      password: item.password,
-      registration_fee: item.registration_fee,
-      letter: item.letter,
+  const modelPermissions = (item) => {
+    const permission = {
+      name: item.name,
+      alias: item.alias,
+      description: item.description,
     };
-    return participants;
+    return permission;
   };
 
   return {
-    modelParticipants,
+    modelPermissions,
   };
 };
